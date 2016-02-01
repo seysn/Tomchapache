fce77928809a887849dfbad88b3720c42b6c6b6a
Nicolas Mauger <n-mauger@hotmail.fr>
2016-02-01 11:15:23 +0100

    update

--- webserver/socket.c
+++ webserver/socket.c
@@ -5,10 +5,43 @@
  * socket.c
 ***********************/
 
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 
 #include "socket.h"
 
-int creer_serveur(int port) {
+int creer_serveur(int port) 
+{
+    struct sockaddr_in saddr ;
+    saddr.sin_family = AF_INET ; /* Socket ipv4 */
+    saddr.sin_port = htons(port); /* Port d ’ écoute */
+    saddr.sin_addr.s_addr=INADDR_ANY ; /* écoute sur toutes les interfaces */
+
+    int socket_serveur ;
+    socket_serveur = socket (AF_INET,SOCK_STREAM,0);
+    
+    if (socket_serveur == -1)
+    {
+        perror ("socket_serveur");
+        return -1; 
+    }
+
+    if (bind(socket_serveur, (struct sockaddr *)& saddr, sizeof(saddr)) == -1)
+    {
+        perror ( " bind socker_serveur " );
+        return -1;
+    }
+
+    if ( listen ( socket_serveur , 10) == -1)
+    {
+        perror ( " listen socket_serveur " );
+        return -1; 
+    }
+
+    /* Utilisation de la socket serveur */
+    
     return 0;
 }
+
