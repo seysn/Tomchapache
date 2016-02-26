7db750a10a8916f73a637041d38a19805a9e2979
Nicolas Seys <seys.nicolas@gmail.com>
2016-02-26 10:16:16 +0100

    checking first line of GET request

--- webserver/main.c
+++ webserver/main.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <ctype.h>
 
 #include "socket.h"
 #include "signal.h"
@@ -26,7 +27,7 @@ int main()
     }
 
     initialiser_signaux();
-    const char * message_bienvenue = "Bonjour, bienvenue sur le serveur\nle plus parfait du monde.\nSur votre droite, vous pourrez voir\nrien qui n\'est plus parfait que\nserveur qui defie la perfection.\nSur votre gauche, pareil.\nNenufar.\nOgnon.";
+    const char * message_bienvenue = "Bonjour, bienvenue sur le serveur\nle plus parfait du monde.\nSur votre droite, vous pourrez voir\nrien qui n\'est plus parfait que\nserveur qui defie la perfection.\nSur votre gauche, pareil.\nNenufar.\nOgnon.\n";
 
     while (1) {
         socket_client = accept(socket_serveur, NULL, NULL);
@@ -44,9 +45,42 @@ int main()
                 sleep(1);
                 write(socket_client, message_bienvenue, strlen(message_bienvenue) + 1);
                 
-                while(fgets(buf, BUF_SIZE, fsocket) != NULL){
-                    fprintf(fsocket, "<Tomchapache> %s",buf);
-                }
+                fgets(buf, BUF_SIZE, fsocket);
+
+		/* On teste si la premiere ligne est correcte */
+		int words = 0, ok = 1;
+		unsigned int i;
+		for (i = 0; i < strlen(buf); i++) {
+		    if (buf[i] == ' ' || buf[i] == '\n') {
+			words++;
+		    }
+		}
+
+		if (words != 3) {
+		    ok = 0;
+		}
+
+		char * tmp = NULL;
+		tmp = strstr(buf, "HTTP/1");
+		if (tmp == NULL) {
+		    ok = 0;
+		} else {
+		    if (tmp[7] != '0' && tmp[7] != '1') {
+			ok = 0;
+		    }
+		}
+		while(fgets(buf, BUF_SIZE, fsocket) != NULL && !strcmp(buf, "\r\n") && !strcmp(buf, "\n"));
+                
+		if (!ok) {
+		    /* La première ligne est incorrecte */
+		    char * msg = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
+		    write(socket_client, msg, strlen(msg));
+		} else {
+		    /* La première ligne est correcte */
+		    char * msg = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 8\r\n\r\n200 OK\r\n";
+		    write(socket_client, msg, strlen(msg));
+		}
+		
                 return EXIT_SUCCESS;
             default:
                 close(socket_client);
