fb75dc6473ed6f47b419b593ca4e9491ca89038c
Nicolas Seys <seys.nicolas@gmail.com>
2016-03-04 10:11:51 +0100

    checking first line is better

--- webserver/main.c
+++ webserver/main.c
@@ -28,8 +28,9 @@ int main()
 
     initialiser_signaux();
     const char * message_bienvenue = "Bonjour, bienvenue sur le serveur\nle plus parfait du monde.\nSur votre droite, vous pourrez voir\nrien qui n\'est plus parfait que\nserveur qui defie la perfection.\nSur votre gauche, pareil.\nNenufar.\nOgnon.\n";
-    const char * message_404 = "Erreur 404, etc etc";
-    
+    const char * message_404 = "404 Not Found";
+    const char * message_400 = "400 Bad Request";
+
     while (1) {
         socket_client = accept(socket_serveur, NULL, NULL);
         if (socket_client == -1) {
@@ -43,55 +44,60 @@ int main()
                 return EXIT_FAILURE;
             case 0:
                 /* On peut maintenant dialoguer avec le client */
-                                
                 fgets(buf, BUF_SIZE, fsocket);
-		printf("%s", buf);
+
 		/* On teste si la premiere ligne est correcte */
 		int words = 0, ok = 1, notfound = 0;
-		unsigned int i;
-		for (i = 0; i < strlen(buf); i++) {
-		    if (buf[i] == ' ' || buf[i] == '\n') {
-			words++;
-		    }
-		}
+		char * currentWord = strtok(buf, " ");
 
-		if (words != 3) {
-		    ok = 0;
-		}
-
-		char * check404 = NULL;
-		char * tmp = NULL;
-		tmp = strstr(buf, "HTTP/1");
-		if (tmp == NULL) {
-		    ok = 0;
-		} else {
-		    if (tmp[7] != '0' && tmp[7] != '1') {
-			ok = 0;
-		    } else {
-			check404 = strtok(buf, " ");
-			check404 = strtok(NULL, " ");
-			if (strcmp(check404, "/")) {
+		/* On teste chaque mot de la première ligne*/
+		while (currentWord != NULL) {
+		    words++;
+		    switch(words) {
+		    case 1:
+			if (strcmp(currentWord, "GET") != 0)
+			    ok = 0;
+			break;
+		    case 2:
+			if (strcmp(currentWord, "/") != 0) {
 			    notfound = 1;
 			}
+			break;
+		    case 3:
+			if (strstr(currentWord, "HTTP/1") == NULL || (currentWord[7] != '0' && currentWord[7] != '1')) {
+			    ok = 0;
+			}
+			break;
+		    default:
+			break;
 		    }
+		    
+		    currentWord = strtok(NULL, " ");
 		}
+
+		if (words != 3)
+		    ok = 0;
+
+		/* On ignore les autres lignes */
 		while(fgets(buf, BUF_SIZE, fsocket) != NULL && !strcmp(buf, "\r\n") && !strcmp(buf, "\n"));
-                
-		if (!ok) {
+		
+		if (ok == 0) {
 		    /* La première ligne est incorrecte */
-		    char * msg = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
+		    char msg[BUF_SIZE];
+		    int len = strlen(message_404);
+		    sprintf(msg, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s\r\n", len, message_400);
 		    write(socket_client, msg, strlen(msg));
 		} else {
 		    /* La première ligne est correcte */
-		    if (notfound) {
+		    if (notfound == 1) {
 			char msg[BUF_SIZE];
 			int len = strlen(message_404);
-			sprintf(msg, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s", len, message_404);
+			sprintf(msg, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s\r\n", len, message_404);
 			write(socket_client, msg, strlen(msg));
 		    } else {
 			char msg[BUF_SIZE];
 			int len = strlen(message_bienvenue);
-			sprintf(msg, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s", len, message_bienvenue);
+			sprintf(msg, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s\r\n", len, message_bienvenue);
 			write(socket_client, msg, strlen(msg));
 		    }
 		}
